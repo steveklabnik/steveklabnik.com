a3c133cdfb3748e911018375ca2bd7215925cc5c
Steve Klabnik <steve@steveklabnik.com>
2020-06-12 16:11:58 -0500

    let's actually use the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,22 +2,11 @@ import Link from 'next/link'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
-const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
-]
-
-const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
-
-export default ({ titlePre = '' }) => {
-  const { pathname } = useRouter()
-
+export default () => {
   return (
     <header>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>Steve Klabnik's website</title>
         <meta name="Steve Klabnik" content="Steve Klabnik's personal website" />
         <link
           rel="alternate"
@@ -26,21 +15,6 @@ export default ({ titlePre = '' }) => {
           href="/atom.rss"
         />
       </Head>
-      <ul>
-        {navItems.map(({ label, page, link }) => (
-          <li key={label}>
-            {page ? (
-              <Link href={page}>
-                <a className={pathname === page ? 'active' : undefined}>
-                  {label}
-                </a>
-              </Link>
-            ) : (
-              <Link href={link}>{label}</Link>
-            )}
-          </li>
-        ))}
-      </ul>
     </header>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import 'normalize.css'
 import 'concrete.css'
 import '../styles/global.css'
+import Header from '../components/header'
 import Footer from '../components/footer'
 
 export default ({ Component, pageProps }) => (
   <>
+    <Header />
     <Component {...pageProps} />
     <Footer />
   </>
