94260f17a6076ee6d3bf831ca7fe49bae767ff31
Steve Klabnik <steve@steveklabnik.com>
2024-04-27 21:06:45 -0500

    Fix RSS ordering
    Fixes #11

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,12 +1,19 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
+  const posts = await pagesGlobToRssItems(import.meta.glob('../pages/writing/*.{md,mdx}'));
+  const items = posts
+    .sort((a, b) => Date.parse(b.pubDate) - Date.parse(a.pubDate))
+    .map(({ pubDate, title, link }) => ({
+      title,
+      link,
+      pubDate: new Date(pubDate),
+    }));
+
   return rss({
     title: 'Steve Klabnik',
     description: 'Steve Klabnik\s blog',
     site: context.site,
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./writing/*.{md,mdx}'),
-    ),
+    items,
   });
 }
