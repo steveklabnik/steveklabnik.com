b2feeb40cefc4bb5ab87c177e2f479caf1e4f49f
Steve Klabnik <steve@steveklabnik.com>
2025-03-17 11:10:05 -0500

    Update some packages
    Hopefully this fixes the actual build on vercel...
    
    also added a link to my bluesky post on "against names", given that I forgot to
    do that.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: 'https://steveklabnik.com',
+  site: "https://steveklabnik.com",
   integrations: [mdx(), tailwind(), react()],
-  adapter: vercel()
+  adapter: vercel(),
 });
