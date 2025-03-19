bef063e4d71a29898606ae0924467aab62163c95
Steve Klabnik <steve@steveklabnik.com>
2025-03-19 14:01:39 -0500

    Add post bodies to RSS feed
    Fixes #24
    
    Sadly, this won't render my MDX components, so that's a bit ugly. Oh well...

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,8 +1,11 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const posts = await await getCollection("blog")
+  const posts = await await getCollection("blog");
   const items = posts
     .sort((a, b) => b.data.pubDate - a.data.pubDate)
     .map((post) => ({
@@ -10,11 +13,14 @@ export async function GET(context) {
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/writing/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
     }));
 
   return rss({
-    title: 'Steve Klabnik',
-    description: 'Steve Klabnik\s blog',
+    title: "Steve Klabnik",
+    description: "Steve Klabniks blog",
     site: context.site,
     items,
   });
