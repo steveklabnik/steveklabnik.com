ef7a7dd05bad60b117f6884d3224df81ff8d25e8
Steve Klabnik <steve@steveklabnik.com>
2025-06-03 22:13:05 -0500

    clarify link comment

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,4 +1,4 @@
-// for legacy reasons, this is also in global.css. 
+// Additional link styling (underline, visited color) lives in global.css
 export default function Link({ text, href }) {
   return (
     <a className="text-blue-500" href={href}>{ text }</a>
