4176251a3a6cdba9a1d50fcbfa4c5ef2362c9009
Seth Rider <seth.rider@gmail.com>
2025-06-10 19:10:49 +0100

    render mdx in rss feed

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,22 +1,30 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
-import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-const parser = new MarkdownIt();
+import { getCollection, render } from "astro:content";
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { getContainerRenderer as getMDXRenderer } from "@astrojs/mdx";
+import { loadRenderers } from "astro:container";
 
 export async function GET(context) {
+  const renderers = await loadRenderers([getMDXRenderer()]);
+  const container = await AstroContainer.create({ renderers });
+
   const posts = await getCollection("blog");
-  const items = posts
-    .sort((a, b) => b.data.pubDate - a.data.pubDate)
-    .map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/writing/${post.slug}/`,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+
+  const items = await Promise.all(
+    posts
+      .sort((a, b) => b.data.pubDate - a.data.pubDate)
+      .map(async (post) => {
+        const { Content } = await render(post);
+
+        return {
+          title: post.data.title,
+          pubDate: post.data.pubDate,
+          description: post.data.description,
+          link: `/writing/${post.slug}/`,
+          content: await container.renderToString(Content),
+        };
       }),
-    }));
+  );
 
   return rss({
     title: "Steve Klabnik",
