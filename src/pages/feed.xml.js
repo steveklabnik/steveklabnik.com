7b81db016cabd4746e145c3dad1a2a47d251d33b
Steve Klabnik <steve@steveklabnik.com>
2025-08-05 22:03:02 -0700

    move content to subdirectories for easier writing

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -20,7 +20,7 @@ export async function GET(context) {
           title: post.data.title,
           pubDate: post.data.pubDate,
           description: post.data.description,
-          link: `/writing/${post.slug}/`,
+          link: `/writing/${post.slug.replace(/^\d{4}-\d{2}\//, '')}/`,
           content: await container.renderToString(Content),
         };
       }),
